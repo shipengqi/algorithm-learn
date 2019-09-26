c9758e996095f0473dc3b5bb3ecdfad75302a1ba
shipengqi <pooky.shipengqi@gmail.com>
2019-09-26 17:36:07 +0800

    binary tree

---
+++ demos/trees/binary_tree.go
@@ -0,0 +1,72 @@
+package trees
+
+type TreeNode struct {
+	Value        int
+	Left         *TreeNode
+	Right        *TreeNode
+}
+
+func PreOrderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	// 没有子节点
+    if root.Left == nil && root.Right == nil {
+    	return []int{root.Value}
+	}
+	var res []int
+	var stack []*TreeNode
+	stack = append(stack, root)
+
+	for len(stack) != 0 {
+		e := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, e.Value)
+		if e.Right != nil {
+			stack = append(stack, e.Right)
+		}
+		if e.Left != nil {
+			stack = append(stack, e.Left)
+		}
+	}
+	return []int{}
+}
+
+func InOrderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	// 没有子节点
+	if root.Left == nil && root.Right == nil {
+		return []int{root.Value}
+	}
+	res := InOrderTraversal(root.Left)
+	res = append(res, root.Value)
+	res = append(res, InOrderTraversal(root.Right)...)
+    return res
+}
+
+func PostOrderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	// 没有子节点
+	if root.Left == nil && root.Right == nil {
+		return []int{root.Value}
+	}
+	var res []int
+	if root.Left != nil {
+		lres := PostOrderTraversal(root.Left)
+		if len(lres) > 0 {
+			res = append(res, lres...)
+		}
+	}
+	if root.Right != nil {
+		rres := PostOrderTraversal(root.Right)
+		if len(rres) > 0 {
+			res = append(res, rres...)
+		}
+	}
+	res = append(res, root.Value)
+	return res
+}
